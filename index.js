4469ab52dd0ba7e6680deab7b903adc1c801ad71
luqman <luqmanito602@gmail.com>
2023-10-20 12:04:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove()
+let newHeader = document.createElement("h1")
+newHeader.setAttribute("id", "victory")
+newHeader.textContent = "Luqman is the champion"
